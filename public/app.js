4c76bcea3a54ee91dfdf06365cfb5b4f71eb2c45
Arslonov Abdulloh <lwcardinal12@gmail.com>
2026-03-08 02:08:56 +0800

    Add app.js with login functionality for room management

---
+++ public/app.js
@@ -0,0 +1,17 @@
+// public/app.js
+
+// Functionality for creating new rooms and joining existing rooms
+
+const createRoom = (roomName) => {
+    // Logic for creating a new room
+    console.log(`Room ${roomName} created`);
+};
+
+const joinRoom = (roomName) => {
+    // Logic for joining existing room
+    console.log(`Joined room ${roomName}`);
+};
+
+// Sample usage:
+createRoom('room1');
+joinRoom('room1');
